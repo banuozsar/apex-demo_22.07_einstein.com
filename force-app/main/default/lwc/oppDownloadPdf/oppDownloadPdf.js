0c73e0a7a42bb66773ec49f3d798761943e1ec83
banuozsar <banuozsar@gmail.com>
2022-09-12 22:29:05 +0300

    oppdownloadpdf_v1

---
+++ force-app/main/default/lwc/oppDownloadPdf/oppDownloadPdf.js
@@ -0,0 +1,27 @@
+import { LightningElement } from "lwc";
+import { loadScript } from "lightning/platformResourceLoader";
+import downloadjs from "@salesforce/resourceUrl/downloadjs";
+import downloadPDF from "@salesforce/apex/PrintJobPDFController.getPdfFileAsBase64String";
+
+export default class OppDownloadPdf extends LightningElement {
+  boolShowSpinner = false;
+  pdfString;
+  downloadPdf() {
+    this.boolShowSpinner = true;
+    downloadPDF({})
+      .then((response) => {
+        var strFile = "data:application/pdf;base64," + response;
+        console.log(response);
+        this.boolShowSpinner = false;
+        window.download(strFile, "sample.pdf", "application/pdf");
+      })
+      .catch((error) => {
+        console.log("Error: " + error.body.message);
+      });
+  }
+  renderedCallback() {
+    loadScript(this, downloadjs)
+      .then(() => console.log("Loaded download.js"))
+      .catch((error) => console.log(error));
+  }
+}
