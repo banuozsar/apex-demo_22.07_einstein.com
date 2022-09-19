cc773d9ae0317f5e927fa9d5078567de4b152111
banuozsar <banuozsar@gmail.com>
2022-09-19 18:04:19 +0300

    message

--- force-app/main/default/lwc/oppCommentAndDownloadPdf/oppCommentAndDownloadPdf.js
+++ force-app/main/default/lwc/oppCommentAndDownloadPdf/oppCommentAndDownloadPdf.js
@@ -1,46 +1,23 @@
-import { LightningElement, api } from "lwc";
-import { loadScript } from "lightning/platformResourceLoader";
-import downloadjs from "@salesforce/resourceUrl/downloadjs";
-import downloadPDF from "@salesforce/apex/PrintJobPDFController.getPdfFileAsBase64String";
-import { NavigationMixin } from "lightning/navigation";
-//import { getRecord, getFieldDisplayValue } from 'lightning/uiRecordApi';
-export default class OppCommentAndDownloadPdf extends NavigationMixin(
-  LightningElement
-) {
+import { LightningElement, api} from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
+
+import ID_FIELD from '@salesforce/schema/Opportunity.Id';
+
+export default class OppCommentAndDownloadPdf extends NavigationMixin(LightningElement) {
   @api recordId;
   @api objectApiName;
 
-  boolShowSpinner = false;
-  pdfString;
+    actionToVFNav() {
+      this[NavigationMixin.GenerateUrl]({
+        type: 'standard__webPage',
+        attributes: {
+          url: '/apex/OppPdf?id=' + this.recordId
+        }
+      }).then(generatedUrl => {
+        window.open(generatedUrl);
+        });
+    }
 
-  downloadPdf() {
-    this.boolShowSpinner = true;
-    downloadPDF({})
-      .then((response) => {
-        var strFile = "data:application/pdf;base64," + response;
-        console.log(response);
-        this.boolShowSpinner = false;
-        window.download(strFile, "sample.pdf", "application/pdf");
-      })
 
-      .catch((error) => {
-        console.log("Error: " + error.body.message);
-      });
-  }
-
-  renderedCallback() {
-    loadScript(this, downloadjs)
-      .then(() => console.log("Loaded download.js"))
-      .catch((error) => console.log(error));
-  }
-  actionToVFNav() {
-    this[NavigationMixin.GenerateUrl]({
-      type: "standard__webPage",
-      attributes: {
-        url: "/apex/OppPdf?id=" + this.recordId
-      }
-    }).then((generatedUrl) => {
-      window.open(generatedUrl);
-    });
-  }
 }
