1ffd9bfb2255e112be0047950b3b070949e50404
banuozsar <banuozsar@gmail.com>
2022-09-19 18:07:19 +0300

    oppCommentAndDownload

--- force-app/main/default/lwc/oppCommentAndDownloadPdf/oppCommentAndDownloadPdf.js
+++ force-app/main/default/lwc/oppCommentAndDownloadPdf/oppCommentAndDownloadPdf.js
@@ -1,23 +1,40 @@
 import { LightningElement, api} from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import { NavigationMixin } from 'lightning/navigation';
+import { CloseActionScreenEvent } from 'lightning/actions';
 
 import ID_FIELD from '@salesforce/schema/Opportunity.Id';
+
+const fields = [ID_FIELD];
 
 export default class OppCommentAndDownloadPdf extends NavigationMixin(LightningElement) {
   @api recordId;
   @api objectApiName;
 
-    actionToVFNav() {
-      this[NavigationMixin.GenerateUrl]({
-        type: 'standard__webPage',
-        attributes: {
-          url: '/apex/OppPdf?id=' + this.recordId
-        }
-      }).then(generatedUrl => {
-        window.open(generatedUrl);
-        });
+  actionToVFNav() {
+    this[NavigationMixin.GenerateUrl]({
+      type: 'standard__webPage',
+      attributes: {
+        url: '/apex/OppPdf?id=' + this.recordId
+      }
+    }).then(generatedUrl => {
+      window.open(generatedUrl);
+      });
     }
 
-
+  handleReset() {
+    const inputFields = this.template.querySelectorAll(
+      'lightning-input-field');
+      if (inputFields) {
+        inputFields.forEach(field => {
+          if(field.name === "comment__c") {
+            field.reset();
+          }
+        });
+      }
+  }
+  
+  closeQuickAction() {
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
 }
