dc66a7e0dfe6b9e120a218957f0ac07509c34baf
banuozsar <banuozsar@gmail.com>
2022-10-29 20:51:31 +0300

    2022.10.29

---
+++ force-app/main/default/aura/MyContactList/MyContactListRenderer.js
@@ -0,0 +1,20 @@
+//Created on 2022.10.29
+
+({
+	myAction : function(component, event, helper) {
+        component.set("v.Columns", [
+            {label:"First Name", fieldName:"FirstName", type:"text"},
+            {label:"Last Name", fieldName:"LastName", type:"text"},
+            {label:"Phone", fieldName:"Phone", type:"phone"}
+        ]);
+
+		var action = component.get("c.getContacts");
+action.setParams({
+    recordId: component.get("v.recordId")
+});
+action.setCallback(this, function(data) {
+    component.set("v.Contacts", data.getReturnValue());
+});
+$A.enqueueAction(action);
+	}
+})
